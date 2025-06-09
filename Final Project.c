ae4dd7ac47d21ea059d4b5bdc5c865d33239168e
User1419666 <kjh100900000@gmail.com>
2025-06-09 16:32:49 +0900

    기분 및 친밀도를 표시합니다.
    기분 및 친밀도를 표시합니다.

--- Final Project.c
+++ Final Project.c
@@ -27,6 +27,28 @@ int prevPos = HOME_POS;
 int turn = 0;
 
 
+int rollDice() {
+    return rand() % 6 + 1;
+}
+
+void printMood() {
+    switch (mood) {
+    case 0: printf("기분이 매우 나쁩니다.\n"); break;
+    case 1: printf("심심해합니다.\n"); break;
+    case 2: printf("식빵을 굽습니다.\n"); break;
+    case 3: printf("골골송을 부릅니다.\n"); break;
+    }
+}
+
+void printIntimacy() {
+    switch (intimacy) {
+    case 0: printf("곁에 오는 것조차 싫어합니다.\n"); break;
+    case 1: printf("간식 자판기 취급입니다.\n"); break;
+    case 2: printf("그럭저럭 쓸 만한 집사입니다.\n"); break;
+    case 3: printf("훌륭한 집사로 인정 받고 있습니다.\n"); break;
+    case 4: printf("집사 껌딱지입니다.\n"); break;
+    }
+}
 
     return 0;
 }
